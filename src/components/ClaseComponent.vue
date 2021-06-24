ff7b698ee863f7390fa9039f322dda6ada5857fb
Federico Blengio <fedeeblengio@gmail.com>
2021-06-23 22:00:44 -0300

    post foro

--- src/components/ClaseComponent.vue
+++ src/components/ClaseComponent.vue
@@ -1,23 +1,81 @@
 <template>
   <div>
+    <button
+      v-if="!alumno"
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target=".bd-example-modal-lg"
+    >
+      Agregar Post
+    </button>
+
     <h1>Grupo {{ datos.idGrupo }} - {{ datos.materia }}</h1>
     <div class="container p-3 my-3 border">
       <vue-headful :title="title" />
 
-      <ckeditor :editor="editor" v-model="editorData"></ckeditor>
+      <div>
+        <h1>Foro</h1>
+        <div v-for="datos in datosForo" :key="datos.id">
+          <div class="contenedorForo">
+            <i
+              class="fas fa-arrow-right"
+              v-on:click="flechas(datos.id, datos.mensaje)"
+            >
+            </i>
+            {{ datos.titulo }}
+          </div>
+          <div class="contenedorForo div" v-bind:id="datos.id"></div>
+        </div>
+      </div>
 
-      <input
-        type="submit"
-        value="Datos"
-        class="btn btn-danger"
-        v-on:click="enviarPost()"
-      />
+      <div
+        class="modal fade bd-example-modal-lg"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="myLargeModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modeloContenedor">
+              <h2>Nuevo Post</h2>
+              <hr />
+              <div>
+                <p>Titulo:</p>
+                <input
+                  type="text"
+                  class="form-control imputCss"
+                  v-model="nuevoPost.titulo"
+                />
+              </div>
 
-      <br>
-    <br>
-    <h1>Foro</h1>
-    <br>
-   <p>{{responseDatos}}</p> 
+              <div class="customImput input-group mb-3">
+                <div class="custom-file">
+                 <VFileInput>
+                   
+                   
+                   
+                   
+                  </VFileInput>
+                </div>
+              </div>
+
+              <ckeditor
+                :editor="editor"
+                v-model="nuevoPost.ckeditor"
+              ></ckeditor>
+
+              <input
+                type="submit"
+                value="Datos"
+                class="btn btn-primary"
+                v-on:click="enviarPost()"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,11 +84,14 @@
 import vueHeadful from "vue-headful";
 import { Global } from "../Global";
 import axios from "axios";
+import JQuery from "jquery";
+import VFileInput from 'v-file-input'
 export default {
   name: "ClaseComponent.vue",
 
   components: {
     vueHeadful,
+    VFileInput,
   },
   data() {
     return {
@@ -42,24 +103,51 @@ export default {
         idUsuario: "",
       },
       responseDatos: "",
-      editorData: "",
+      nuevoPost: {
+        ckeditor: "",
+        titulo: "",
+        archivo: null,
+      },
       editor: "",
-      datosforo: "",
+      datosForo: "",
+      alumno: false,
     };
   },
   mounted() {
     if (!localStorage.getItem("auth_token")) {
       this.$router.push("/home");
     }
-    let usuario = JSON.parse(window.atob(localStorage.getItem("auth_token")));
-    this.datos.idUsuario = usuario.username;
-    this.datos.idMateria = this.$route.params.idMateria;
-    this.datos.materia = this.$route.params.materia;
-    this.datos.idGrupo = this.$route.params.idGrupo;
+    this.guardarRutas();
     this.getDatos();
-    this.traerDatosForo();
   },
+
   methods: {
+    flechas(id, mensaje) {
+      let $ = JQuery;
+      let div2 = document.getElementById(id);
+      let mensaje2 = window.atob(mensaje);
+
+      if (div2.style.display === "none") {
+        div2.style.display = "block";
+        $(id).removeClass("fa-arrow-right").addClass("fa-arrow-down");
+        div2.innerHTML = mensaje2;
+      } else {
+        div2.style.display = "none";
+        $(id).removeClass("fa-arrow-down").addClass("fa-arrow-right");
+      }
+    },
+
+    guardarRutas() {
+      let usuario = JSON.parse(window.atob(localStorage.getItem("auth_token")));
+      if (usuario.ou === "Alumno") {
+        this.alumno = true;
+      }
+      this.datos.idUsuario = usuario.username;
+      this.datos.idMateria = this.$route.params.idMateria;
+      this.datos.materia = this.$route.params.materia;
+      this.datos.idGrupo = this.$route.params.idGrupo;
+    },
+
     getDatos() {
       axios
         .get(
@@ -71,26 +159,22 @@ export default {
             this.datos.idGrupo
         )
         .then((res) => {
-          //console.log('servicios', res.status);
           if (res.status == 200) {
             this.responseDatos = res.data;
+            this.traerDatosForo(this.responseDatos.idForo);
           } else {
             alert("no se pudo conectar");
           }
         });
     },
-    traerDatosForo() {
-       alert("a")
-      axios
-        .get( Global.urlSitio + "foro-grupo?idForo="+ this.responseDatos.idForo)
-        .then((res) => {
-          //console.log('servicios', res.status);
-          if (res.status == 200) {
-            this.datosforo = res.data;
-          } else {
-            alert("no se pudo conectar");
-          }
-        });
+    traerDatosForo(idForo) {
+      axios.get(Global.urlSitio + "foro-grupo?idForo=" + idForo).then((res) => {
+        if (res.status == 200) {
+          this.datosForo = res.data;
+        } else {
+          alert("no se pudo conectar");
+        }
+      });
     },
     enviarPost() {
       let config = {
@@ -101,9 +185,12 @@ export default {
       let parametros = {
         idForo: this.responseDatos.idForo,
         idUsuario: this.responseDatos.idProfesor,
-        mensaje: window.btoa(this.editorData),
+        mensaje: window.btoa(this.nuevoPost.ckeditor),
+        archivo: this.nuevoPost.archivo,
+        titulo: this.nuevoPost.titulo,
       };
-
+      console.log(parametros.archivo);
+      alert("gggg");
       axios
         .post(Global.urlSitio + "foro", parametros, config)
         .then((response) => {
--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import Vuelidate from 'vuelidate';
 import FlashMessage from "@smartweb/vue-flash-message";
 import CKEditor from 'ckeditor4-vue';
 
+
 import HomeComponent from './components/HomeComponent.vue';
 import LoginComponent from './components/LoginComponent.vue';
 import ErrorComponent from './components/ErrorComponent.vue';
 import miPerfilComponent from './components/miPerfilComponent.vue';
-import foroComponent from './components/foroComponent.vue';
+
 import gruposComponent from './components/gruposComponent.vue';
 import ClaseComponent from './components/ClaseComponent.vue';
 import MateriasAlumnoComponent from './components/MateriasAlumnoComponent.vue'
+
 
 Vue.config.productionTip = false
 
@@ -21,13 +23,13 @@ Vue.use(VueRouter);
 Vue.use(Vuelidate);
 Vue.use(CKEditor);
 
+
 //acá declaramos todas nuestras rutas de navegación
 const routes = [
     { path: '/', redirect: '/home' },
     { path: '/home', name: 'home', component: HomeComponent },
     { path: '/login', name: 'login', component: LoginComponent },
     { path: '/miPerfil', name: 'miPerfil', component: miPerfilComponent },
-    { path: '/foro', name: 'Foro', component: foroComponent },
     { path: '/mis-grupos', name: 'mis-grupos', component: gruposComponent },
     { path: '/clases/:idGrupo/:materia/:idMateria', name: 'clase-foro', component: ClaseComponent },
     { path: '/materias', name: 'alumno-materias', component: MateriasAlumnoComponent},
