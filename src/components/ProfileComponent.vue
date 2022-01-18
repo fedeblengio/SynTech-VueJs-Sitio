4aa8e16253d62ec67faf91bf797ead7613e0cdea
Fede <fedeeblengio@gmail.com>
2022-01-17 21:19:53 -0300

    cambio de mi perfil solo la parte de fondo de perfil

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -1,20 +1,27 @@
 <template>
-<div class="contenedorDiv">
-<vue-headful :title="title" />
- <SectionLeft></SectionLeft>
+  <div class="contenedorDiv">
+    <vue-headful :title="title" />
+    <SectionLeft></SectionLeft>
     <div class="feed">
       <div class="feed_header">
         <h2>Mi Perfil</h2>
       </div>
-
       <div class="boxText">
-        <h1>ANASHE</h1>
-      
-     
+        <div class="imgProfile">
+          <div class="imgContenedorProfile">
+            <img
+              src="https://pbs.twimg.com/profile_images/1366466342354751491/JyhZpbtu_400x400.jpg"
+            />
+            <div class="textImg">
+              <h3>Federico Blengio</h3>
+              <h4>Cambiar foto</h4>
+            </div>
+          </div>
+        </div>
       </div>
-    </div> 
- <SectionRight></SectionRight>
-</div>
+    </div>
+    <SectionRight></SectionRight>
+  </div>
 </template>
 <script>
 import vueHeadful from "vue-headful";
