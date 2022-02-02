6a18805777881cf9cebe30f7293833c5cc65583f
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-02-02 16:51:59 -0300

    Entrar a jitsi alumno profe

--- src/components/CalendarioComponent.vue
+++ src/components/CalendarioComponent.vue
@@ -1,4 +1,4 @@
-<template>
+  <template>
   <div class="contenedorDiv">
     <vue-headful :title="title" />
     <SectionLeft></SectionLeft>
@@ -6,15 +6,29 @@
       <div class="feed_header">
         <h2>Calendario de Clases</h2>
       </div>
-      <div></div>
+      <div>
+        <calendar :attributes="attrs" is-expanded />
+      </div>
+      <div v-for="clase in listClasesVirtuales" :key="clase.id" class="post">
+        <div class="post_body">
+          {{ clase.idGrupo }}
+          {{ clase.materia }}
+          {{ clase.fecha_inicio }}
+          {{ clase.fecha_fin }}
+        </div>
+        <button class="boxText_btn" v-on:click="entrarJitsi(clase)">
+          Entrar
+        </button>
+      </div>
     </div>
     <SectionRight></SectionRight>
   </div>
 </template>
 <script>
 import vueHeadful from "vue-headful";
-/* import { Global } from "../Global";
-import axios from "axios"; */
+import "vue-event-calendar/dist/style.css";
+import { Global } from "../Global";
+import axios from "axios";
 import JQuery from "jquery";
 import SectionLeft from "./SectionLeft.vue";
 import SectionRight from "./SectionRight.vue";
@@ -30,23 +44,58 @@ export default {
     return {
       title: "Profile",
       usuario: JSON.parse(window.atob(localStorage.getItem("auth_token"))),
+      listClasesVirtuales: "",
+      attrs: [
+        {
+          key: "today",
+          highlight: true,
+          dates: new Date(),
+        },
+      ],
     };
   },
   mounted() {
-    if (this.usuario.ou == "Profesor") {
+    /*  if (this.usuario.ou == "Profesor") {
       this.calendarioProfesor();
     } else {
       this.calendarioAlumno();
-    }
+    } */
+    this.clasesVirtualesCreadas();
   },
   methods: {
-    calendarioAlumno() {
+    entrarJitsi(clase){
+      let url = "https://meet.jit.si/"+ window.btoa(clase)
+      return window.open(url,"_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=800,height=500").focus();
+    },
+    clasesVirtualesCreadas() {
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+          token: Global.token,
+        },
+      };
+
+      axios
+        .get(
+          Global.urlSitio +
+            "agenda-clase?idUsuario=" +
+            this.usuario.username +
+            "&ou=" +
+            this.usuario.ou,
+          config
+        )
+        .then((res) => {
+          if (res.status == 200) {
+            this.listClasesVirtuales = res.data;
+          }
+        });
+    },
+    /*    calendarioAlumno() {
       alert("ES ALUMNO");
     },
     calendarioProfesor() {
       alert("ES PROFESOR");
-    },
-
+    }, */
   },
 };
 </script>
