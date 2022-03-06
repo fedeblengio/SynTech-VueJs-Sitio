fe1ee4e24fa74e3e6c25e5bda6ce2166096f3f69
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-03-05 23:47:02 -0300

    Nuevos servidores

--- src/Global.js
+++ src/Global.js
@@ -1,6 +1,6 @@
 export var Global = {
-    urlBackOffice: 'http://syntech2021.ddns.net:8000/api/',
-    urlSitio: 'http://syntech2021.ddns.net:8001/api/',
+    urlBackOffice: 'http://190.64.41.140:8000/api/',
+    urlSitio: 'http://190.64.41.140:8001/api/',
     token: localStorage.getItem("auth_token"),
     tituloSitio: 'LMS',
 }
