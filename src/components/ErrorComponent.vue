77af355ca67afff70b986ed12afbe08e7258bd20
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-05-09 15:46:25 -0300

    error Component en ing

--- src/components/ErrorComponent.vue
+++ src/components/ErrorComponent.vue
@@ -2,9 +2,13 @@
   <div class="contenedorDiv">
     <vue-headful :title="title" />
     <SectionLeft></SectionLeft>
-    <div class="feed" >
-      <img  style="display:block;margin:30px auto" src="https://new.edmodo.com/static/media/oops_404.cd0dcfe0.png" alt="">
-      <h3 style="text-align: center">Pagina no encontrada</h3>
+    <div class="feed">
+      <img
+        style="display: block; margin: 30px auto"
+        src="https://new.edmodo.com/static/media/oops_404.cd0dcfe0.png"
+        alt=""
+      />
+      <h3 style="text-align: center">{{languages.paginaNoEncontrada}}</h3>
     </div>
     <SectionRight></SectionRight>
   </div>
@@ -14,19 +18,36 @@ import vueHeadful from "vue-headful";
 /* import { Global } from "../Global";
 import axios from "axios";
 import JQuery from "jquery"; */
+import language from "../assets/lang/errorComponent.json";
+
 import SectionLeft from "./SectionLeft.vue";
 import SectionRight from "./SectionRight.vue";
 export default {
   name: "error-component",
   components: {
     vueHeadful,
-     SectionLeft,
+    SectionLeft,
     SectionRight,
   },
   data() {
     return {
-      title: "Pagina no encontrada",
+      title: "",
+      lang: localStorage.getItem("lang"),
+      language: "",
     };
+  },
+  mounted() {
+    this.selectLanguage();
+  },
+  methods: {
+    selectLanguage() {
+      if (localStorage.getItem("lang") == "es") {
+        this.language = language.es;
+      } else {
+        this.language = language.en;
+      }
+      this.title = this.language.title;
+    },
   },
 };
 </script>
