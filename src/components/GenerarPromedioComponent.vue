ffb23810ca01fb3a726b0510440aa6e250fd1ed0
Federico Blengio <fedeeblengio@gmail.com>
2022-05-12 21:20:08 -0300

    style promedio

--- src/components/GenerarPromedioComponent.vue
+++ src/components/GenerarPromedioComponent.vue
@@ -3,7 +3,40 @@
     <vue-headful :title="title" />
     <SectionLeft></SectionLeft>
     <div class="feed">
-      {{language.hola}}
+      <div class="feed_header" style="border-bottom: 1px solid var(--background);margin-bottom:10px;">
+        <h2>Promedio</h2>
+      </div>
+       
+       <div class="GenerarCont" style="width: 95%;margin:0 auto; ">
+         <p>
+           Lista de Alumno
+         </p>
+         <button class="btnGenerar">Descrgar</button>
+       </div>
+       <div style='margin-top:10px; text-align: center;'>
+         <table class="table" >
+        <thead>
+        <tr>
+          <td>Nombre</td>
+          <td>Promedio</td>
+          <td>Aporbado</td>
+        </tr>
+      </thead>
+      <tbody>
+      <tr>
+          <td>Mark</td>
+          <td>Mark</td>
+          <td>Mark</td>   
+      </tr>
+      <tr>
+          <td>Mark</td>
+          <td>Mark</td>
+          <td>Mark</td>   
+      </tr>
+  
+  </tbody>
+</table>
+       </div>
     </div>
     <SectionRight></SectionRight>
   </div>
