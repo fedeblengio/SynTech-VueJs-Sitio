0c3f4dd8f13ba0cf20f8d5ae05a7df21c7b98b94
Agustin Kuster <61338886+aguskuster@users.noreply.github.com>
2022-10-15 15:53:40 -0300

    add middlewares

--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ import Calendar from 'v-calendar/lib/components/calendar.umd'
 import DatePicker from 'v-calendar/lib/components/date-picker.umd'
 import VuePictureSwipe from 'vue-picture-swipe';
 import VueSweetalert2 from 'vue-sweetalert2';
+import auth_student from './middleware/auth_student';
+import auth_teacher from './middleware/auth_teacher';
+import auth from './middleware/auth'; 
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 import ListadoTareasEntregadasComponent from './components/ListadoTareasEntregadasComponent.vue';
@@ -48,31 +51,31 @@ Vue.component('vue-picture-swipe', VuePictureSwipe);
 //acá declaramos todas nuestras rutas de navegación   <router-link
 
 const routes = [
-    { path: '/', redirect: '/home' },
+    { path: '/', redirect: '/home'  },
     { path: '/login', name: 'login', component: LoginComponent },
-    { path: '/home', name: 'home', component: HomeComponent },
-    { path: '/profile/:idUsuario', name: 'profile', component: ProfileComponent },
-    { path: '/visitar-perfil/:idUsuario', name: 'visitar-perfil', component: VisitarPerfilComponent },
-    { path: '/noticias', name: 'listado-noticias', component: NoticiasComponent },
-    { path: '/listado-tareas/:idGrupo/:materia/:idMateria', name: 'listado-tareas', component: ListadoTareasComponent },
-    { path: '/listado-tareas/:idGrupo/:materia/:idMateria/:tareas_vencidas', name: 'listado-tareas-vencidas', component: ListadoTareasComponent },
-    { path: '/tarea/:materia/:idTarea/:re_hacer', name: 'tarea-seleccionada', component: TareaSeleccionadaComponent },
-    { path: '/listado-entregas/:idGrupo/:idMateria/:idTareas/', name: 'entregas', component: ListadoTareasEntregadasComponent },
-    { path: '/calificar-alumno/:idAlumnos/:idTareas/:re_entrega', name: 'calificar-alumnos', component: CalificarAlumnoComponent },
-    { path: '/visualizar-tarea/:idTareas/:idAlumnos/', name: 'visualizar-tarea', component: VisualizarTareaComponent },
-    { path: '/materias', name: 'materias-component', component: MateriasComponent },
-    { path: '/materia-seleccionada/:idGrupo-:idMateria/:materia', name: 'materia-seleccionada', component: MateriasSeleccionadaComponent },
-    { path: '/agenda-virtual', name: 'agenda-virtual', component: AgendaVirtualComponent },
-    { path: '/listado-usuarios/:idGrupo/:idMateria/:materia', name: 'listado-usuarios', component: ListadoUsuariosComponent },
-    { path: '/calendario', name: 'calendario', component: CalendarioComponent },
-    { path: '/registro-clases', name: 'registro-clases', component: RegistroClasesComponent },
-    { path: '/pasar-lista/:idGrupo/:idMateria/:idClase', name: 'pasaje-lista', component: PasajeDeListaComponent },
-    { path: '/calificaciones', name: 'calificaciones-alumno', component: ListadoCalificacionesComponent },
-    { path: '/PDF/:idClase/:materia/:fecha', name: 'PDF', component: PDFComponent },
-    { path: '/historial-faltas', name: 'historial-faltas', component: RegistroDeFaltasComponent },
-    { path: '/cambioPwd', name: 'cambiopwd', component: CambiarContraComponent },
-    { path: '/generarPromedio/:idGrupo/:Materia/:idMateria', name: 'materia-generarPromedio', component: GenerarPromedioComponent },
-    { path: '*', name: 'error404', component: ErrorComponent },
+    { path: '/home', name: 'home', component: HomeComponent , meta: { middleware: auth}},
+    { path: '/profile/:idUsuario', name: 'profile', component: ProfileComponent , meta: { middleware: auth} },
+    { path: '/visitar-perfil/:idUsuario', name: 'visitar-perfil', component: VisitarPerfilComponent , meta: { middleware: auth}},
+    { path: '/noticias', name: 'listado-noticias', component: NoticiasComponent , meta: { middleware: auth_teacher} },
+    { path: '/listado-tareas/:idGrupo/:materia/:idMateria', name: 'listado-tareas', component: ListadoTareasComponent , meta: { middleware: auth} },
+    { path: '/listado-tareas/:idGrupo/:materia/:idMateria/:tareas_vencidas', name: 'listado-tareas-vencidas', component: ListadoTareasComponent , meta: { middleware: auth} },
+    { path: '/tarea/:materia/:idTarea/:re_hacer', name: 'tarea-seleccionada', component: TareaSeleccionadaComponent , meta: { middleware: auth} },
+    { path: '/listado-entregas/:idGrupo/:idMateria/:idTareas/', name: 'entregas', component: ListadoTareasEntregadasComponent , meta: { middleware: auth_teacher}},
+    { path: '/calificar-alumno/:idAlumnos/:idTareas/:re_entrega', name: 'calificar-alumnos', component: CalificarAlumnoComponent, meta: { middleware: auth_teacher}  },
+    { path: '/visualizar-tarea/:idTareas/:idAlumnos/', name: 'visualizar-tarea', component: VisualizarTareaComponent , meta: { middleware: auth_teacher} },
+    { path: '/materias', name: 'materias-component', component: MateriasComponent , meta: { middleware: auth}},
+    { path: '/materia-seleccionada/:idGrupo-:idMateria/:materia', name: 'materia-seleccionada', component: MateriasSeleccionadaComponent , meta: { middleware: auth}},
+    { path: '/agenda-virtual', name: 'agenda-virtual', component: AgendaVirtualComponent , meta: { middleware: auth_teacher}},
+    { path: '/listado-usuarios/:idGrupo/:idMateria/:materia', name: 'listado-usuarios', component: ListadoUsuariosComponent , meta: { middleware: auth} },
+    { path: '/calendario', name: 'calendario', component: CalendarioComponent , meta: { middleware: auth}},
+    { path: '/registro-clases', name: 'registro-clases', component: RegistroClasesComponent , meta: { middleware: auth_teacher} },
+    { path: '/pasar-lista/:idGrupo/:idMateria/:idClase', name: 'pasaje-lista', component: PasajeDeListaComponent  , meta: { middleware: auth_teacher} },
+    { path: '/calificaciones', name: 'calificaciones-alumno', component: ListadoCalificacionesComponent  , meta: { middleware: auth_student} },
+    { path: '/PDF/:idClase/:materia/:fecha', name: 'PDF', component: PDFComponent , meta: { middleware: auth_teacher} },
+    { path: '/historial-faltas', name: 'historial-faltas', component: RegistroDeFaltasComponent , meta: { middleware: auth_teacher}},
+    { path: '/cambioPwd', name: 'cambiopwd', component: CambiarContraComponent , meta: { middleware: auth} },
+    { path: '/generarPromedio/:idGrupo/:Materia/:idMateria', name: 'materia-generarPromedio', component: GenerarPromedioComponent , meta: { middleware: auth_teacher}},
+    { path: '*', name: 'error404', component: ErrorComponent , meta: { middleware: auth} },
 ];
 
 
