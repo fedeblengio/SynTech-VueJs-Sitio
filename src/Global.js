cfcea14cab837666bdd23f31e498cb1a50a3e3e3
Agustin Kuster <61338886+aguskuster@users.noreply.github.com>
2023-08-30 20:06:05 -0300

    Update Global.js

--- src/Global.js
+++ src/Global.js
@@ -1,5 +1,5 @@
 export var Global = {
-    urlSitio: 'http://192.168.50.137:8001/api/',
+    urlSitio: 'http://10.106.5.150:8001/api/',
     token: localStorage.getItem("auth_token"),
     tituloSitio: 'LMS',
     spinnerUrl: 'https://i.imgur.com/rBo6t55.gif',
